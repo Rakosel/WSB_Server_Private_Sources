ccd66ecbf9ae6235ce9966dafead4b9a24d942ea
root <root@BilSrvStation.BilSrvWork>
2023-12-04 21:22:17 +0400

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,  начинающиеся с «» будут проигнорированы, а пустое сообщение  отменяет процесс коммита.
     Коммитер:  root <root@BilSrvStation.BilSrvWork>
    
     На ветке mainESP32
     Ваша ветка обновлена в соответствии с «origin/mainESP32».
    
     Изменения, которые будут включены в коммит:
    	новый файл:    !_Html/GaugeMeter.js
    	новый файл:    !_Html/MPesp32.html
    	изменено:      !_Html/MPesp32_bez_JS_main_sidebar.html
    	новый файл:    !_Html/MPesp32_bez_JS_main_sidebar.zip
    	изменено:      .gitignore
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/BokehFrost.jpg
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/GaugeMeter.js
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/LICENSE
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/doc.html
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/knockout_demo.html
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/knockout_demo.zip
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/radial-component.html
    	новый файл:    Customizable-Animated-jQuery-HTML5-Gauge-Meter-Plugin/readme.md
    	изменено:      WSB_page_main.html
    	изменено:      wsb_script_2_2_2.js

--- wsb_script_2_2_2.js
+++ wsb_script_2_2_2.js
@@ -1,4 +1,4 @@
-// upd82a8 STABLE trim with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js
+// upd83a1 STABLE trim with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js
 //
 //
 //					https://javascriptcompressor.com/
@@ -1288,7 +1288,7 @@ function txjs_ua(s, d) {
     //rs = to(refr, 3);
     //refr();
 }
-
+	// $(document).ready(function ()
 window.onload = function () {
     $(".bt0st").attr("value", "off");
     $(".navia").addClass("list-group-item list-group-item-action bg-light border");
@@ -1296,6 +1296,27 @@ window.onload = function () {
     $("#esp_urx").val("");
     rs = setInterval(refr_rtc, 3000);
     i = 0;
+	
+	                // Initialize GaugeMeter plugin
+                $(".GaugeMeter").gaugeMeter();
+
+                // Bind new handler to init and update gauges.
+                ko.bindingHandlers.gaugeValue = {
+                    init: function(element, valueAccessor) {
+                        $(element).gaugeMeter({ percent: ko.unwrap(valueAccessor()) });
+                    },
+                    update: function(element, valueAccessor) {
+                        $(element).gaugeMeter({ percent: ko.unwrap(valueAccessor()) });
+                    }
+                };
+
+                // Create view model with inital gauge value 15mph
+                // Use observable for easy update.
+                var myViewModel = {
+                    Percent: ko.observable(15)
+                };
+                ko.applyBindings(myViewModel);
+	
     //$(".bsn0").collapse('show');
     smgh();
 };
